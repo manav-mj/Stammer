6ec7cddcd46521f92239b11a7246be492e6b4099
Manav Jain <manav.mj.jain@gmail.com>
2020-06-10 19:44:37 +0530

    UI : DashboardActivity title

--- app/src/main/java/com/mohit/stammer/DashboardActivity.java
+++ app/src/main/java/com/mohit/stammer/DashboardActivity.java
@@ -3,12 +3,9 @@ package com.mohit.stammer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -51,7 +48,7 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard);
+        setContentView(R.layout.activity_dashboard);
     }
 }
 
