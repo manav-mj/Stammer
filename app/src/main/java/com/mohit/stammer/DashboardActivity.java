bd839202f99e46fd64319ef5609e058bff5ddbc9
Manav Jain <manav.mj.jain@gmail.com>
2020-07-10 18:06:55 +0530

    UI : DashboardActivity

--- app/src/main/java/com/mohit/stammer/DashboardActivity.java
+++ app/src/main/java/com/mohit/stammer/DashboardActivity.java
@@ -13,43 +13,38 @@ import com.google.firebase.auth.FirebaseAuth;
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.logout_menu,menu);
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    public void logout(View view){
-
-        FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(this ,MainActivity.class);
-        startActivity(intent);
-        getSharedPreferences("stammer", MODE_PRIVATE).edit().remove("user_id").apply();
-        Toast.makeText(this, "Logged out successfully" , Toast.LENGTH_SHORT).show();
-    }
-
-    public void speech_therapy(View view){
-
-        Intent intent = new Intent(this , speech_therapy.class);
-        startActivity(intent);
-
-    }
-
-    public void caseStudy(View view){
-        Intent intent = new Intent(this ,CaseStudy.class);
-        startActivity(intent);
-    }
-
-    public void spreadAwareness(View view){
-        Intent intent = new Intent(this ,SpreadAwareness.class);
-        startActivity(intent);
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.logout_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        getSharedPreferences("stammer", MODE_PRIVATE).edit().remove("user_id").apply();
+        Toast.makeText(this, "Logged out successfully", Toast.LENGTH_SHORT).show();
+    }
+
+    public void speech_therapy(View view) {
+        Intent intent = new Intent(this, speech_therapy.class);
+        startActivity(intent);
+    }
+
+    public void caseStudy(View view) {
+        Intent intent = new Intent(this, CaseStudy.class);
+        startActivity(intent);
+    }
+
+    public void spreadAwareness(View view) {
+        Intent intent = new Intent(this, SpreadAwareness.class);
+        startActivity(intent);
     }
 }
 
